3a1c83866ba563b742f3bce18c85b1b66e5afdee
wiljo7 <wiljo7hdz@gmail.com>
2023-03-31 15:42:26 -0400

    inicio

---
+++ streamlit01.py
@@ -0,0 +1,30 @@
+
+import pygame
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from datetime import datetime, date, time, timedelta
+import calendar
+import sqlite3
+from time import sleep
+import matplotlib.patches as mpatches
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt  # instalar
+import matplotlib as mpl
+import os
+import requests
+import mysql.connector
+import string
+import xlrd
+import pandas as pd
+import altair as alt
+###
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import numpy as np
+
+
+st.write('Hola')
